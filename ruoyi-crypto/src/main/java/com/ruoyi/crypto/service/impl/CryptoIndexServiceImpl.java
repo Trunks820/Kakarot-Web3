28ad3ef20abe7911196a061cec4cbdaaa03ab75e
Trunks820 <Kakarot820@outlook.com>
2025-06-29 16:58:23 +0800

    优化机器人api

--- ruoyi-crypto/src/main/java/com/ruoyi/crypto/service/impl/CryptoIndexServiceImpl.java
+++ ruoyi-crypto/src/main/java/com/ruoyi/crypto/service/impl/CryptoIndexServiceImpl.java
@@ -9,6 +9,8 @@ import com.ruoyi.crypto.mapper.CryptoCaQueryRecordMapper;
 import com.ruoyi.crypto.mapper.CryptoUserMapper;
 import com.ruoyi.crypto.service.CryptoIndexService;
 import com.ruoyi.crypto.utils.BotApiUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -18,6 +20,8 @@ import java.util.Map;
 
 @Service
 public class CryptoIndexServiceImpl implements CryptoIndexService {
+
+    private static final Logger log = LoggerFactory.getLogger(CryptoIndexServiceImpl.class);
 
     @Resource
     private CryptoCaQueryRecordMapper cryptoCaQueryRecordMapper;
@@ -38,20 +42,33 @@ public class CryptoIndexServiceImpl implements CryptoIndexService {
 
     @Override
     public AjaxResult getBotStatus(String type) {
+        log.info("开始获取机器人状态，类型: {}", type);
         String url = "http://tgalert-app:5000/api/status";
-//        String url = "http://108.160.135.74:5000/api/status";
+        log.info("调用API: {}", url);
+        
         AjaxResult botApi = BotApiUtils.getBotApi(url);
         if(botApi.isError()){
+            log.error("API调用失败: {}", botApi.get("msg"));
             return botApi;
         }
 
         try {
-            String jsonString = botApi.get("data") + "";
-            // 移除可能的前后空白字符
-            jsonString = jsonString.trim();
+            log.info("API调用成功，开始解析响应数据");
+            // 直接获取已经解析好的data对象
+            Object dataObj = botApi.get("data");
+            log.debug("响应数据类型: {}", dataObj.getClass().getSimpleName());
             
-            // 解析完整的Python API响应
-            JSONObject fullResponse = JSONUtil.parseObj(jsonString);
+            // 如果是JSONObject，直接使用；如果是字符串，则解析
+            JSONObject fullResponse;
+            if (dataObj instanceof JSONObject) {
+                fullResponse = (JSONObject) dataObj;
+                log.debug("数据已经是JSONObject格式");
+            } else {
+                // 如果是字符串，则解析
+                String jsonString = dataObj.toString();
+                log.debug("原始响应数据: {}", jsonString);
+                fullResponse = JSONUtil.parseObj(jsonString);
+            }
 
             // 获取bots信息
             JSONObject bots = fullResponse.getJSONObject("bots");
@@ -59,52 +76,74 @@ public class CryptoIndexServiceImpl implements CryptoIndexService {
             // 获取指定类型的机器人信息
             JSONObject botInfo = bots.getJSONObject(type);
             botInfo.put("name", type);
+            log.info("机器人状态获取成功，类型: {}, 状态: {}", type, botInfo.getStr("status"));
             return AjaxResult.success(botInfo);
         } catch (Exception e) {
+            log.error("解析机器人状态数据失败，类型: {}, 错误: {}", type, e.getMessage(), e);
             return AjaxResult.error("解析机器人状态数据失败：" + e.getMessage());
         }
     }
 
     @Override
     public AjaxResult restartTgBot(String name) {
+        log.info("开始重启机器人: {}", name);
         String url = "http://tgalert-app:5000/api/bot/" + name + "/restart";
+        log.info("调用重启API: {}", url);
+        
         AjaxResult ajaxResult = BotApiUtils.postBotApi(url);
         if(ajaxResult.isError()){
+            log.error("重启机器人失败，机器人: {}, 错误: {}", name, ajaxResult.get("msg"));
             return ajaxResult;
         } else{
+            log.info("重启机器人成功: {}", name);
             return AjaxResult.success();
         }
     }
 
     @Override
     public AjaxResult startTgBot(String name) {
+        log.info("开始启动机器人: {}", name);
         String url = "http://tgalert-app:5000/api/bot/" + name + "/start";
+        log.info("调用启动API: {}", url);
+        
         AjaxResult ajaxResult = BotApiUtils.postBotApi(url);
         if(ajaxResult.isError()){
+            log.error("启动机器人失败，机器人: {}, 错误: {}", name, ajaxResult.get("msg"));
             return ajaxResult;
         } else{
+            log.info("启动机器人成功: {}", name);
             return AjaxResult.success();
         }
     }
 
     @Override
     public AjaxResult stopTgBot(String name) {
+        log.info("开始停止机器人: {}", name);
         String url = "http://tgalert-app:5000/api/bot/" + name + "/stop";
+        log.info("调用停止API: {}", url);
+        
         AjaxResult ajaxResult = BotApiUtils.postBotApi(url);
         if(ajaxResult.isError()){
+            log.error("停止机器人失败，机器人: {}, 错误: {}", name, ajaxResult.get("msg"));
             return ajaxResult;
         } else{
+            log.info("停止机器人成功: {}", name);
             return AjaxResult.success();
         }
     }
 
     @Override
     public AjaxResult startAllTgBot() {
+        log.info("开始启动所有机器人");
         String url = "http://tgalert-app:5000/api/bots/start_all";
+        log.info("调用启动所有机器人API: {}", url);
+        
         AjaxResult ajaxResult = BotApiUtils.postBotApi(url);
         if(ajaxResult.isError()){
+            log.error("启动所有机器人失败，错误: {}", ajaxResult.get("msg"));
             return ajaxResult;
         } else{
+            log.info("启动所有机器人成功");
             return AjaxResult.success();
         }
     }
